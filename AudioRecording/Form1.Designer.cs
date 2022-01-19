94f9a6169a375198ebabcb4ecaec815fa6e0f6c2
sunger7 <sunger7@163.com>
2022-01-20 01:22:52 +0800

    添加项目文件。

---
+++ AudioRecording/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace AudioRecording
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 192;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(192, 450);
+            this.treeView1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80.88889F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.11111F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(604, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 2);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 367);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(598, 80);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 60);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.tableLayoutPanel1.SetColumnSpan(this.pictureBox1, 2);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(598, 358);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ AudioRecording/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace AudioRecording
+{
+    public partial class Form1 : Form
+    {
+        string outputFolder;
+        string outputFilePath;
+        WaveInEvent waveIn;
+        WaveFileWriter writer = null;
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+        bool closing = false;
+        bool reDraw = false;
+        bool isPlay = false;//是否在播放文件
+        byte[] samples = null;
+        float[] audioPlayingData = null;
+        public Form1()
+        {
+            InitializeComponent();
+            outputFolder = Path.Combine(Environment.CurrentDirectory, "NAudio");
+            Directory.CreateDirectory(outputFolder);
+            //outputFilePath = Path.Combine(outputFolder, "recorded.wav");
+            waveIn = new WaveInEvent();
+            //Console.WriteLine(waveIn.WaveFormat.SampleRate+"_"+ waveIn.WaveFormat.BitsPerSample+"_"
+            //    + waveIn.WaveFormat.AverageBytesPerSecond);
+            waveIn.DataAvailable += (s, a) =>
+            {
+                writer.Write(a.Buffer, 0, a.BytesRecorded);
+                if (!reDraw)
+                {
+                    unsafe
+                    {
+                        samples = new byte[a.BytesRecorded];
+                        System.Array.Copy(a.Buffer, samples, a.BytesRecorded);
+                        Console.WriteLine(a.BytesRecorded);
+                        Thread mythread = new Thread(new ParameterizedThreadStart(DrawWave));
+                        mythread.Start(samples);
+                    }
+
+                }
+                
+                if (writer.Position > waveIn.WaveFormat.AverageBytesPerSecond * 30)
+                {
+                    waveIn.StopRecording();
+                }
+            };
+            waveIn.RecordingStopped += (s, a) =>
+            {
+                writer?.Dispose();
+                writer = null;
+                button1.Enabled = true;
+                button2.Enabled = false;
+                if (closing)
+                {
+                    waveIn.Dispose();
+                }
+            };
+            this.FormClosing += (s, a) => { 
+                closing = true; 
+                waveIn.StopRecording();
+                if(outputDevice!=null)
+                outputDevice.Stop(); 
+            };
+            this.SizeChanged += (s, a) =>
+            {
+                if (outputDevice == null)
+                    DrawPlayingWave(audioPlayingData, audioPlayingData.Length);
+            };
+
+            InitializeTreeView();
+            treeView1.NodeMouseDoubleClick += treeView1_NodeMouseDoubleClick;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(outputDevice != null || outputDevice != null)
+            {
+                outputDevice.Dispose();
+                outputDevice = null;
+                audioFile.Dispose();
+                audioFile = null;
+                isPlay = false;
+            }
+
+            if (true)
+            {
+                int i = 1;
+                do
+                {
+                    string fileName = "recorded" + i + ".wav";
+                    outputFilePath = Path.Combine(outputFolder, fileName);
+                    i++;
+                } while (File.Exists(outputFilePath));
+
+                writer = new WaveFileWriter(outputFilePath, waveIn.WaveFormat);
+                waveIn.StartRecording();
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button2.Text = "Stop";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!isPlay)
+            {
+                waveIn.StopRecording();
+                TreeNode curNode = treeView1.Nodes[0];
+                var fi2 = new FileInfo(outputFilePath);
+                curNode.Nodes.Add(fi2.FullName, fi2.Name);
+            }
+            else
+            {
+                if(button2.Text == "Pause On") { 
+                    outputDevice.Play();
+                    button2.Text = "Playing";
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                }
+                else
+                {
+                    outputDevice.Pause();
+                    button2.Text = "Pause On";
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                }
+
+            }
+
+        }
+        // Populates a TreeView control with example nodes. 
+        private void InitializeTreeView()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(outputFolder);
+            treeView1.BeginUpdate();
+            TreeNode curNode = treeView1.Nodes.Add(directoryInfo.Name);
+            //treeView1.Nodes.Add("Parent");
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                curNode.Nodes.Add(file.FullName, file.Name);
+            }
+            treeView1.EndUpdate();
+            treeView1.Nodes[0].Expand();
+        }
+        private void DrawWave(object bytes)
+        {
+            byte[] nbytes = (byte[])(bytes);
+            float[] shorts = new float[nbytes.Length/2];
+            for (int i = 0; i < shorts.Length; i++)
+            {
+                shorts[i] = (short)(nbytes[i * 2] | (nbytes[i * 2 + 1] << 8))/32768f;
+                // absolute value 
+                //if (shorts[i] < 0) shorts[i] = -shorts[i];
+                // is this the max value?
+                
+            }
+            float max = shorts.Max(), min = shorts.Min();
+            reDraw = true;
+            
+            {
+                // Draw next line and...
+                pictureBox1.Image = null;
+                int wid = pictureBox1.ClientSize.Width;
+                int hgt = pictureBox1.ClientSize.Height;
+
+                // Draw with double-buffering.
+                Bitmap bm = new Bitmap(wid, hgt);
+                using (Graphics gr = Graphics.FromImage(bm))
+                {
+                    //DrawButterfly(gr, wid, hgt);
+                    PointF[] points = new PointF[ Convert.ToInt16( shorts.Length / 5)];
+                    for (int i = 0;i< points.Length;i++)
+                    {
+                        points[i].Y = (shorts[i * 5] - min) / (max - min) * hgt;
+                        points[i].X = (i*1.0f)/ (points.Length*1.0f) * wid;
+                    }
+
+                    gr.DrawLines(new Pen(Brushes.Red), points);
+                }
+                this.Invoke(new Action(() =>
+                {
+                    pictureBox1.Image = bm;
+                    //pictureBox1.Refresh();
+                }));
+
+                // Draw without double-buffering.
+                //DrawButterfly(picCanvas2.CreateGraphics(), wid, hgt);
+                // ... Remember the location
+                
+            }
+            reDraw = false;
+            
+        }
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (outputDevice != null || audioFile != null)
+            {
+                outputDevice.Dispose(); outputDevice = null; audioFile.Dispose(); audioFile = null;
+            }
+            string fileName = e.Node.Name;
+            if (outputDevice == null)
+            {
+                outputDevice = new WaveOutEvent();
+                outputDevice.PlaybackStopped += (s, a) => { if (closing) { outputDevice.Dispose(); outputDevice = null; audioFile.Dispose(); audioFile = null; } };
+            }
+            if (audioFile == null)
+            {
+                audioFile = new AudioFileReader(fileName);
+                outputDevice.Init(audioFile);
+                long len = audioFile.Length;
+                audioPlayingData = new float[len / 4];
+                audioFile.Read(audioPlayingData, 0, Convert.ToInt32(len / 4));
+                audioFile.Position = 0;
+            }
+            outputDevice.Play();
+            button1.Enabled = false;
+            button2.Enabled = true;
+            button2.Text = "Playing";
+            isPlay = true;
+
+
+        }
+        private void DrawPlayingWave(float[] data,long location)
+        {
+            if (location > data.Length) location = data.Length;
+            float max = data.Max(), min = data.Min();
+
+            {
+                // Draw next line and...
+                pictureBox1.Image = null;
+                int wid = pictureBox1.ClientSize.Width;
+                int hgt = pictureBox1.ClientSize.Height;
+
+                // Draw with double-buffering.
+                Bitmap bm = new Bitmap(wid, hgt);
+                using (Graphics gr = Graphics.FromImage(bm))
+                {
+                    //DrawButterfly(gr, wid, hgt);
+                    PointF[] points = new PointF[Convert.ToInt32(data.Length / 5.0)];
+                    PointF[] pointsPast = new PointF[Convert.ToInt32(Math.Floor( location*1.0 / 5))];
+                    for (int i = 0; i < points.Length; i++)
+                    {
+                        points[i].Y = (data[i * 5] - min) / (max - min) * hgt;
+                        points[i].X = (i * 1.0f) / (points.Length * 1.0f) * wid;
+                        if(i< pointsPast.Length)
+                        {
+                            pointsPast[i].Y = points[i].Y;
+                            pointsPast[i].X = points[i].X;
+                        }
+                    }
+
+                    gr.DrawLines(new Pen(Brushes.DarkCyan), points);
+                    gr.DrawLines(new Pen(Brushes.Red), pointsPast);
+                }
+                
+                this.Invoke(new Action(() =>
+                {
+                    pictureBox1.Image = bm;
+                    //pictureBox1.Refresh();
+                }));
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (outputDevice != null)
+                if (outputDevice.GetPosition() > 0) {
+                    DrawPlayingWave(audioPlayingData, Convert.ToInt32(outputDevice.GetPosition() / 4));
+                    //Console.WriteLine(audioPlayingData.Length + "_" + Convert.ToInt32( outputDevice.GetPosition()/4));
+                    if(outputDevice.GetPosition() > audioPlayingData.Length*4)
+                    {
+                        outputDevice.Stop();
+                        outputDevice.Dispose(); outputDevice = null; audioFile.Dispose(); audioFile = null;
+                        button1.Enabled = true;
+                        button2.Enabled = false;
+                        button2.Text = "Stop";
+                    }
+                }
+                    
+        }
+    }
+}
